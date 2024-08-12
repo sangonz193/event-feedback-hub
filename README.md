8febfa4d5e9968f1c7a18f53fb9d16c0efe86eb5
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-08-12 15:54:19 -0300

    update readme

--- README.md
+++ README.md
@@ -6,7 +6,15 @@
    ```bash
    npm install
    ```
-2. Run both the frontend and backend with the following command, also at the root of the project.
+2. Run the DB migrations with the following command in the _apps/api_ directory.
+   ```bash
+   npx prisma migrate reset
+   ```
+3. Generate prisma client files.
+   ```bash
+   npx prisma generate
+   ```
+4. Run both the frontend and backend with the following command at the root of the project.
    ```bash
    npm run dev
    ```
--- apps/client/src/routes/index.lazy.tsx
+++ apps/client/src/routes/index.lazy.tsx
@@ -1,10 +1,16 @@
 import { gql } from "@/__generated__"
+import {
+  EmptyState,
+  EmptyStateIcon,
+  EmptyStateTitle,
+} from "@/components/empty-state"
 import { useLayoutOptions } from "@/components/layout/use-layout-options"
 import { EventFormDialog } from "@/domains/event/event-form-dialog"
 import { EventItem } from "@/domains/event/event-item"
 import { cn } from "@/lib/utils"
 import { useQuery } from "@apollo/client"
 import { createLazyFileRoute } from "@tanstack/react-router"
+import { CalendarIcon } from "lucide-react"
 import { useMemo, useState } from "react"
 import { createHtmlPortalNode, OutPortal } from "react-reverse-portal"
 
@@ -26,7 +32,8 @@ function Index() {
     createHtmlPortalNode({ attributes: { class: cn("flex") } }),
   )
 
-  const { data } = useQuery(GET_EVENTS)
+  const { data, loading } = useQuery(GET_EVENTS)
+  const showEmptyState = !loading && !data?.events?.length
 
   const [open, setOpen] = useState(false)
 
@@ -47,6 +54,16 @@ function Index() {
             <EventItem key={event.id} event={event} />
           ))}
         </div>
+      )}
+
+      {showEmptyState && (
+        <EmptyState>
+          <EmptyStateIcon>
+            <CalendarIcon />
+          </EmptyStateIcon>
+
+          <EmptyStateTitle>There are no events to show</EmptyStateTitle>
+        </EmptyState>
       )}
 
       <EventFormDialog
